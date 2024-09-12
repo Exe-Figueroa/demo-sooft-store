80393242debb700a4e57a47169570871a0b54786
Facundo Figueroa <ffigueroa@nogadev.com>
2024-09-11 21:11:59 -0300

    02_ Create new methods

--- src/context/DataContextProvider.jsx
+++ src/context/DataContextProvider.jsx
@@ -1,21 +1,50 @@
 import { createContext, useState } from "react";
 
-export const DataProvider = createContext(); // .Consumer .Provider
+export const DataProvider = createContext();
 
 export const DataContextProvider = ({ children }) => {
   const [order, setOrder] = useState([]);
-  console.log(order);
-  
-  const cleanOrder = ()=>{
+  const cleanOrder = () => {
     setOrder([]);
-  }
-  const addNewProduct = (product)=> {
-    setOrder(prevValue => [...prevValue, product])
-  }
-  const obj = { order, cleanOrder, addNewProduct }
-  return (
-    <DataProvider.Provider value={obj}>
-      {children}
-    </DataProvider.Provider>
-  );
+  };
+
+  const updateProductByIndex = (newProduct, index) => {
+    const firstPart = order.slice(0, index);
+    const secondPart = order.slice(index + 1, order.length);
+    setOrder([...firstPart, newProduct, ...secondPart]);
+  };
+
+  const addNewProduct = (newProduct) => {
+    setOrder((prevValue) => [...prevValue, newProduct]);
+  };
+
+  const upsertProduct = (newProduct) => {
+    const indexProduct = order.findIndex((p) => p?.id === newProduct.id);
+
+    if (newProduct.qty == 0 && indexProduct >= 0) {
+      if (order.length === 1) {
+        cleanOrder();
+      } else {
+        deleteProductByIndex(indexProduct);
+      }
+    } else if (indexProduct >= 0) {
+      updateProductByIndex(newProduct, indexProduct);
+    } else if (newProduct.qty > 0) {
+      addNewProduct(newProduct);
+    }
+  };
+
+  const deleteProductByIndex = (productIndex) => {
+    const newOrder = order.filter((_, index) => index !== productIndex);
+    setOrder(newOrder);
+  };
+
+  const obj = {
+    order,
+    cleanOrder,
+    addNewProduct,
+    upsertProduct,
+    updateProductByIndex,
+  };
+  return <DataProvider.Provider value={obj}>{children}</DataProvider.Provider>;
 };
