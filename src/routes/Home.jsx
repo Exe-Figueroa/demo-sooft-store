37e8cc0deb55b7cf84e198a21f4bc91d950887ab
Facundo Figueroa <ffigueroa@nogadev.com>
2024-09-12 22:16:14 -0300

    12_ OrderModal implemented

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,18 @@
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import { OrderModal } from "../components/OrderModal";
 import { ProductsContainer } from "../components/Products";
 
 export const Home = () => {
   return (
-    <main className="flex flex-col items-center gap-4">
+    <div className="flex flex-col items-center gap-4">
       <Header />
+    <main className="w-full px-4 max-w-screen-2xl md:px-0 flex items-center justify-center ">
+      <OrderModal />
       <ProductsContainer />
-      <Footer/>
     </main>
+      <Footer/>
+
+    </div>
   );
 };
