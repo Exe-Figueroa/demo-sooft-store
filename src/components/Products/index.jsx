05133f9168d4ab495c5fc2424e09a59bec5b41e1
Facundo Figueroa <ffigueroa@nogadev.com>
2024-09-12 22:09:38 -0300

    05_ Added media queries into ProductContainer section

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -2,7 +2,7 @@ import dummyProducts from "../../data/dummyProducts.json"
 import { ProductCard } from "./ProductCard";
 export const ProductsContainer = () => {
   return (
-    <section className="grid grid-cols-auto-fill-270-300 gap-2 w-9/12">
+    <section className="grid grid-cols-auto-fill-270-300 gap-2 md:gap-4 w-9/12">
       {dummyProducts.map(product => <ProductCard key={product.id} product={product}/>)}
     </section>
   );
