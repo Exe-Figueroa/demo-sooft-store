9142ea1e204cbbbd01ab43fc99a237d8320ac739
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-09-30 20:24:23 -0300

    07_ IMplement getAllProducts into home.jsx

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,9 +1,21 @@
-import dummyProducts from "../../data/dummyProducts.json"
+import { useEffect, useState } from "react";
 import { ProductCard } from "./ProductCard";
+import { ProductService } from "../../products-management/application/product.service";
+import { ProductSupabaseRepository } from "../../products-management/infraestructure/product.repository";
+import { supabaseClient } from "../../libs/supabaseConnection";
+
+const productService = new ProductService(
+  new ProductSupabaseRepository(supabaseClient)
+);
+
 export const ProductsContainer = () => {
+  const [products, setProducts] = useState([]);
+useEffect(() => {
+    productService.getAllProducts().then(setProducts).catch(console.error);
+  }, []);
   return (
     <section className="grid grid-cols-auto-fill-270-300 gap-2 md:gap-4 w-9/12">
-      {dummyProducts.map(product => <ProductCard key={product.id} product={product}/>)}
+      {products.map(product => <ProductCard key={product.id} product={product}/>)}
     </section>
   );
 };
